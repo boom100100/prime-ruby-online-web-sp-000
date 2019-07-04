e20a7cbbced0a1c0136289da5776504636995291
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 20:54:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,19 @@ def prime?(number)# Add  code here!
   if number <= 1 #integers 1 or less
     return false
   end
-  cmath.sqrt(number)
 
   if number.even? && number != 2 #even numbers
     return false
-  elsif !number.even? #odd numbers
-
-  else
-    return true
   end
+
+  n = 2
+  squareRoot = cmath.sqrt(number)
+  while n > squareRoot
+    result = number / n
+    return true if n.is_a? Integer
+    n = n + 1
+  end
+  return false
+
+  
 end
