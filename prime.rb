46fffc55ab481a1dfa080ff7eab84ff909fe2100
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 21:02:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,11 @@ def prime?(number)# Add  code here!
 
   if number.even? && number != 2 #even numbers
     return false
+  elsif number.even?
+    return true
   end
 
-  n = 0
+  n = 1
   comparisonValue = Math.sqrt(number)
   while n < comparisonValue
     result = number / n
