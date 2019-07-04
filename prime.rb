193a28c3cd763cefc02735a0b368ff6ab2a845d7
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 20:49:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@ def prime?(number)# Add  code here!
     return false
   end
   cmath.sqrt(number)
-  
+
   if number.even? && number != 2 #even numbers
     return false
   elsif !number.even? #odd numbers
-    
+
   else
     return true
   end
