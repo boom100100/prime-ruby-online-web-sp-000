79b7f984ada3666c8d199c6044c22e50027faff1
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 20:49:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
-def prime?# Add  code here!
-
+def prime?(number)# Add  code here!
+  if number <= 1 #integers 1 or less
+    return false
+  end
+  cmath.sqrt(number)
+  
+  if number.even? && number != 2 #even numbers
+    return false
+  elsif !number.even? #odd numbers
+    
+  else
+    return true
+  end
 end
